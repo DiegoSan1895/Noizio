e224dd0fce018a6a3dbea0cf3792039d095f77e8
SanDiego <xyq_since1895@163.com>
2016-04-04 10:07:23 +0800

    fix some bugs

---
+++ Noizio/DSTableCell.m
@@ -0,0 +1,78 @@
+//
+//  DSTableCell.m
+//  Noizio
+//
+//  Created by DiegoSan on 4/4/16.
+//  Copyright © 2016 DiegoSan. All rights reserved.
+//
+
+#import "DSTableCell.h"
+#import "YYKit.h"
+#define kCellHeight 64
+
+@implementation DSTableCell
+
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    self.backgroundColor = [UIColor whiteColor];
+    self.size = CGSizeMake(kScreenWidth, kCellHeight);
+    self.contentView.size = self.size;
+    
+    _avatar = [UIImageView new];
+    _avatar.size = CGSizeMake(kCellHeight / 2, kCellHeight / 2);
+    _avatar.backgroundColor = [UIColor clearColor];
+    _avatar.center = CGPointMake(kCellHeight / 2, kCellHeight / 2);
+    _avatar.contentMode = UIViewContentModeScaleAspectFit;
+    [self.contentView addSubview:_avatar];
+    
+    _nameLabel = [UILabel new];
+    _nameLabel.size = CGSizeMake(kScreenWidth - kCellHeight, kCellHeight / 2);
+    _nameLabel.backgroundColor = [UIColor clearColor];
+    _nameLabel.right = self.contentView.right;
+    [self.contentView addSubview:_nameLabel];
+    
+    __weak typeof(self) _self = self;
+    
+    _volumSlider = [UISlider new];
+    _volumSlider.size = _nameLabel.size;
+    _volumSlider.width -= 26;
+    _volumSlider.tintColor = [UIColor blackColor];
+    _volumSlider.left = _nameLabel.left;
+    _volumSlider.bottom = self.contentView.bottom;
+    _volumSlider.value = 0.4;
+    [_volumSlider setThumbImage:[UIImage imageNamed:@"Line"] forState:UIControlStateNormal];
+    [_volumSlider addBlockForControlEvents:UIControlEventValueChanged block:^(id  _Nonnull sender) {
+        [_self.delegate sliderValueChanged:_self];
+    }];
+    [self.contentView addSubview:_volumSlider];
+    
+    UIImageView *bottomLine = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"line1"]];
+    bottomLine.size = CGSizeMake(kScreenWidth, 1);
+    [self.contentView addSubview:bottomLine];
+    
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithActionBlock:^(id  _Nonnull sender) {
+        [_self.delegate cellWasClicked:_self];
+    }];
+    [self.contentView addGestureRecognizer:tap];
+    
+    return self;
+}
+
+- (void)prepareForReuse{
+
+}
+
+- (void)changeToPauseState{
+    _avatar.image = [UIImage imageNamed:_pauseAvatarIamge];
+    _nameLabel.alpha = 0.3;
+    _volumSlider.alpha = 0.3;
+}
+
+- (void)changeToPlayingState{
+    _avatar.image = [UIImage imageNamed:_playingAvatarIamge];
+    _nameLabel.alpha = 1;
+    _volumSlider.alpha = 1;
+}
+@end
---
+++ Noizio/PlayerControl.m
@@ -0,0 +1,33 @@
+//
+//  PlayerControl.m
+//  Noizio
+//
+//  Created by DiegoSan on 4/4/16.
+//  Copyright © 2016 DiegoSan. All rights reserved.
+//
+
+#import "PlayerControl.h"
+
+@implementation PlayerControl
+
+- (instancetype)init{
+    self = [super init];
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(playbackFinished:) name:AVPlayerItemDidPlayToEndTimeNotification object:_playerItem];
+    return self;
+}
+
+- (void)playbackFinished: (NSNotification *)notifacation{
+    [_player seekToTime:CMTimeMake(0, 1)];
+    [_player play];
+}
+
+- (void)dealloc{
+    [[NSNotificationCenter defaultCenter]removeObserver:self];
+}
+
+- (void)setVolum:(float)volum{
+    _volum = volum;
+    _player.volume = volum;
+}
+@end
